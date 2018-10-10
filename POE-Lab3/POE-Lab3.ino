3eff481c5f7d4ef1bfd1ebd025d79f8ba0cbeff9
Cassandra Overney <coverney@olin.edu>
2018-10-09 20:52:57 -0400

    Commented two sensor p controller, serial to text, and analyze data code

--- POE-Lab3/POE-Lab3.ino
+++ POE-Lab3/POE-Lab3.ino
@@ -1,7 +1,8 @@
 /* 
-This 
+This script is a Kp controller for our two sensor setup. It contains a serial interface,
+start/stop button, and a limitSpeed helper function.   
 */
-// Import necessary libraries
+
 #include <TFT.h>
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
@@ -10,36 +11,32 @@ This
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();  
 
-// Select which 'port' M1, M2, M3 or M4. In this case, M1
+// Select which 'port' M1, M2, M3 or M4. We used M1 and M2.
 Adafruit_DCMotor *motorLeft = AFMS.getMotor(1);
 Adafruit_DCMotor *motorRight = AFMS.getMotor(2);
-const int IR_SENSOR1 = A0; //sensor to left of tape
-const int IR_SENSOR2 = A1; //sensor to right of tape
-const int MAX_REFLECT = 990; //will need to double check these values
-const int MIN_REFLECT = 800;
-const int CALIBRATION = 0;
-uint8_t maxSpeed = 20;
-int leftSpeed = 20;
-int rightSpeed = 20;
-float Kp = 1;
-bool running = true;
-const int BUTTON = 8;
-
-// You can also make another motor on port M2
-//Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(2);
+const int IR_SENSOR1 = A0; // Sensor to left of tape
+const int IR_SENSOR2 = A1; // Sensor to right of tape
+int maxSpeed = 20; // Maximum speed set to 20 after testing
+int leftSpeed = maxSpeed; // Initialize left motor speed to maxSpeed
+int rightSpeed = maxSpeed; // Initialize right motor speed to maxSpeed
+float Kp = 1; // Kp constant, set to 1 after extensive testing
+bool running = true; // Button state that determines whether to power to motors
+const int BUTTON = 8; // Button digital pin at pin 8
 
 void setup() {
-  Serial.begin(9600);           // set up Serial library at 9600 bps
-  Serial.println("Adafruit Motorshield v2 - DC Motor test!");
-
-  AFMS.begin();  // create with the default frequency 1.6KHz
-  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+  Serial.begin(9600); // Set up Serial library at 9600 bps
+  AFMS.begin();  // Create with the default frequency 1.6KHz
 }
 
 void loop() {
+  // If message sent to serial, then corresponding variable is updated
+  // Variable to change is determined by the first character in message 
   if (Serial.available() > 0){
+    // Read in serial message as String
     String new_char = Serial.readString();
+    // Extract the new number, part of string after first character
     float input_command = new_char.substring(1).toFloat();
+    // Determine which variable needs to be updated
     switch(new_char[0]){
       case '*':
         //Updating maxSpeed
@@ -53,37 +50,35 @@ void loop() {
         Serial.print("+ Message: ");
         Serial.println(Kp);
         break;
-      case ',':
-        // D const = input_command;
-        Serial.print(", Message: ");
-        Serial.println(input_command);
-        break;
     }
   }
-  uint8_t i;
-
+  // If button pressed, then change state of running variable. 
   if (digitalRead(BUTTON)){
     running = !running;
-    delay(50);
+    delay(50); // Delay used to prevent debouncing
   }
+
+  // If running is true, then read in sensor values and calculate motor speeds
+  // using the difference in sensor values multiplied by the Kp constant
   if (running) {
-    motorLeft->run(FORWARD);
-    motorRight->run(BACKWARD);
-    int sensor1 = analogRead(IR_SENSOR1) + CALIBRATION;
-    int sensor2 = analogRead(IR_SENSOR2);
-    float diff = sensor1 - sensor2;
-    int deltaSpeed = round(diff * Kp);
-    //if diff > 0 (sensor1 > sensor2), then the left sensor is touching the black tape, so the car should turn left.
-    //if diff < 0 (sensor1 < sensor2), then the right sensor is touching the black tape, so the car should turn right.
-    //new speed of left wheel should always be subtracted by deltaSpeed
-    leftSpeed = (20 - deltaSpeed);
-    //new speed of right wheel should always by added with deltaSpeed
-    rightSpeed = (20 + deltaSpeed);
-//    leftSpeed = round(map(analogRead(IR_SENSOR), MIN_REFLECT, MAX_REFLECT, 0, maxSpeed)) & 0x00FF;
-//    delay(500);
-//    rightSpeed = (maxSpeed - leftSpeed) & 0x00FF;
+    motorLeft->run(FORWARD); // Moving left motor forward
+    motorRight->run(BACKWARD); // Since right motor mounted backwards, BACKWARD means moving forward
+    int sensor1 = analogRead(IR_SENSOR1); // Read sensor1(left) value
+    int sensor2 = analogRead(IR_SENSOR2); // Read sensor2(right) value
+    float diff = sensor1 - sensor2; // Calculate difference in sensor values
+    int deltaSpeed = round(diff * Kp); // Multiply difference by Kp constant to get difference in speed of motors
+    // deltaSpeed refers to left motor speed subtracted by right motor speed
+    // if diff > 0 (sensor1 > sensor2), then the left sensor is touching the black tape, so the car should turn left
+    // if diff < 0 (sensor1 < sensor2), then the right sensor is touching the black tape, so the car should turn right
+    // To find updated leftSpeed, subtract maxSpeed by deltaSpeed
+    leftSpeed = (maxSpeed - deltaSpeed);
+    // To find updated rightSpeed, add maxSpeed to deltaSpeed
+    rightSpeed = (maxSpeed + deltaSpeed);
+    // Make sure leftSpeed and rightSpeed are between 0 and maxSpeed by calling limitSpeed
     leftSpeed = limitSpeed(leftSpeed, maxSpeed, 0);
     rightSpeed = limitSpeed(rightSpeed, maxSpeed, 0);
+    // Printing data to Serial Monitor, which is saved in txt file
+    // Format: sensor1 value,sensor2 value,left speed,right speed
     Serial.print(sensor1);
     Serial.print(",");
     Serial.print(sensor2);
@@ -91,19 +86,7 @@ void loop() {
     Serial.print(leftSpeed);
     Serial.print(",");
     Serial.println(rightSpeed);
-//    Serial.print("Sensor value 1: ");
-//    Serial.println(sensor1);
-//    Serial.print("Sensor value 2: ");
-//    Serial.println(sensor2);
-//    Serial.print("Diff sensor value and deltaspeed: ");
-//    Serial.print(diff);
-//    Serial.print(", ");
-//    Serial.println(deltaSpeed);
-//    Serial.print("LEFT SPEED: ");
-//    Serial.println(leftSpeed);
-//    Serial.print("RIGHT SPEED: ");
-//    Serial.println(rightSpeed);
-//    delay(500);
+    // Update left and right motor speeds
     motorLeft-> setSpeed(leftSpeed & 0x00FF);
     motorRight-> setSpeed(rightSpeed & 0x00FF);
   }
@@ -127,49 +110,6 @@ int limitSpeed(int initialSpeed, int maxSpeed, int minSpeed) {
   if (initialSpeed < minSpeed) {
     return minSpeed;
   }
-  // Return the original value if it within the range
+  // Return the original value if it is within the range
   return initialSpeed;
-}
-
-int calibrate(int scanSpeed, int sensor, int steps, bool findAverage, bool debugMode) {
-  /* 
-   *  This function is designed to scan the line at the beginning of the course to generate its own
-   *  threshold values for the IR sensor. It takes the following arguments:
-   *    scanSpeed:    Speed (0-255) of the motor during the scan
-   *    sensor:       Pin number of the sensor to be used in the scan
-   *    steps:        Number of points used to calculate reflectance of the ground
-   *    findAverage:  Toggle to switch between finding the average reflectance of the ground. If
-   *                  true, the function will return the average reflectance. If false, the fuction
-   *                  will return the minimum reflectance found over the scan interval
-   *    debugMode:    Toggle for print statements
-   */
-
-  // Initialize Variables
-  int lightValues = 0;
-    
-  // Move off the line
-  motorLeft->run(BACKWARD);
-  motorLeft->setSpeed(scanSpeed);
-  delay(500);
-
-  // Start the scan of the ground
-  for (int i = 0; i < steps; i++) {
-    motorLeft-> setSpeed(scanSpeed);
-    
-    if (findAverage) {
-      lightValues += (analogRead(sensor) / steps);
-    } else if (analogRead(sensor) > lightValues) {
-      lightValues = analogRead(sensor);
-    }
-    delay(20);
-  }
-  // Return to rough start position
-  motorLeft->run(FORWARD);
-  motorLeft->setSpeed(scanSpeed);
-  delay(500 + 20 * steps);
-  motorLeft->setSpeed(0);
-  
-  if (debugMode) {Serial.print("Measured Light Value is: "); Serial.println(lightValues);}
-
-  return lightValues;
 }
